02d01a7084f37aa4d50ec73315884fbf07559f4c
José Augusto Q. C. Gomes <jose.gomes3@estudante.ifms.edu.br>
2019-08-10 18:06:49 -0400

    dados prontos

---
+++ test/NumerosExtensos.java
@@ -0,0 +1,20 @@
+
+public class NumerosExtensos {
+
+    private final static String[] unidades = {
+        "um","dois","tres","quatro","cinco","seis","sete","oito","nove","dez","onze","doze","treze","quatorze","quinze","dezesseis","dezessete","dezoito","dezenove"
+    };
+    private final static String[] dezenas = {
+        "vinte","trinta","quarenta","cinquenta","sessenta","setenta","oitenta","noventa","cem"
+    };
+    private final static String[] centanas = {
+        "cento","duzentos","trezentos","quatrocentos","cincocentos","seiscentos","setecentos","oitocentos","novecentos"
+    };
+    private final static String[] cardinais = {
+        "mil","milhão","bilhão","trilhão","quatrilhão","quintilhão","sextilhão","septilhão","octilhão","nonilhão","decilhão","unidecilhão","duodecilhão","tredecilhão"
+    };
+    private final static String[] cardinais_plural = {
+        "mil","milhões","bilhões","trilhões","quatrilhões","quintilhões","sextilhões","septilhões","octilhões","nonilhões","decilhões","unidecilhões","duodecilhões","tredecilhões"
+    };
+
+}
