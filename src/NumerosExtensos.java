88a84b950aefcbca4b0ca33ecd86ead2163cfefc
José Augusto Q. C. Gomes <jose.gomes3@estudante.ifms.edu.br>
2019-08-10 21:54:34 -0400

    biginteger

--- src/NumerosExtensos.java
+++ src/NumerosExtensos.java
@@ -1,5 +1,9 @@
 
+import java.math.BigInteger;
+
 public class NumerosExtensos {
+
+    private static BigInteger MAX_VALUE = BigInteger.valueOf(10).pow(45).subtract(BigInteger.ONE);
 
     private final static String[] unidades = {
         "um", "dois", "tres", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"
@@ -17,22 +21,41 @@ public class NumerosExtensos {
         "mil", "milhões", "bilhões", "trilhões", "quatrilhões", "quintilhões", "sextilhões", "septilhões", "octilhões", "nonilhões", "decilhões", "unidecilhões", "duodecilhões", "tredecilhões"
     };
 
+    public static String paraExtenso(int numero) {
+        return paraExtenso(BigInteger.valueOf(numero));
+    }
+
+    public static String paraExtenso(byte numero) {
+        return paraExtenso(BigInteger.valueOf(numero));
+    }
+
+    public static String paraExtenso(short numero) {
+        return paraExtenso(BigInteger.valueOf(numero));
+    }
+
     public static String paraExtenso(long numero) {
+        return paraExtenso(BigInteger.valueOf(numero));
+    }
+
+    public static String paraExtenso(BigInteger numero) {
         String extenso = "";
-        if (numero == 0) {
+        if (numero.equals(BigInteger.ZERO)) {
             return "zero";
         }
-        if (numero < 0) {
-            numero = -numero;
+        if (numero.compareTo(BigInteger.ZERO) < 0) {
+            numero = numero.abs();
             extenso += "menos ";
         }
-        if (numero < 1000) {
-            if (numero <= 100) {
-                if (numero < 20) {
-                    return unidades[(int) numero - 1];
+        if (numero.compareTo(MAX_VALUE) > 0) {
+            return "Número muito grande";
+        }
+        if (numero.compareTo(BigInteger.valueOf(1000)) < 0) {
+            if (numero.compareTo(BigInteger.valueOf(100)) <= 0) {
+                if (numero.compareTo(BigInteger.valueOf(20)) < 0) {
+                    return unidades[numero.subtract(BigInteger.ONE).intValue()];
                 } else {
-                    int dezena = (int) numero / 10 - 1;
-                    int resto = (int) numero % 10;
+                    int dezena = numero.divide(BigInteger.valueOf(10)).subtract(BigInteger.ONE).intValue();
+                    int resto = numero.mod(BigInteger.valueOf(10)).intValue();
                     extenso += dezenas[dezena - 1];
                     if (resto > 0) {
                         extenso += " e " + unidades[resto - 1];
@@ -40,28 +63,31 @@ public class NumerosExtensos {
                     return extenso;
                 }
             } else {
-                int centena = (int) numero / 100;
-                int resto = (int) numero % 100;
+                int centena = numero.divide(BigInteger.valueOf(100)).intValue();
+                BigInteger resto = numero.mod(BigInteger.valueOf(100));
                 extenso += centenas[centena - 1];
-                if (resto > 0) {
+                if (resto.compareTo(BigInteger.ZERO) > 0) {
                     extenso += " e " + paraExtenso(resto);
                 }
                 return extenso;
             }
         } else {
-            int c = (String.valueOf(numero).length() - 1) / 3;
-            long maxc = (long) Math.pow(10, c * 3);
-            int qtd_cardinais = (int) (numero / maxc);
-            int resto = (int) (numero % maxc);
-            if (qtd_cardinais > 1 || c > 1) {
+            int c = (numero.toString().length() - 1) / 3;
+            BigInteger maxc = BigInteger.valueOf(10).pow(c * 3);
+            BigInteger qtd_cardinais = numero.divide(maxc);
+            BigInteger resto = numero.mod(maxc);
+            if (qtd_cardinais.compareTo(BigInteger.ONE) > 0 || c > 1) {
                 extenso += paraExtenso(qtd_cardinais) + " ";
             }
-            extenso += qtd_cardinais > 1 ? cardinais_plural[c - 1] : cardinais[c - 1];
-            if (resto > 0) {
+            extenso += qtd_cardinais.compareTo(BigInteger.ONE) > 0 ? cardinais_plural[c - 1] : cardinais[c - 1];
+            if (resto.compareTo(BigInteger.ZERO) > 0) {
                 extenso += " e " + paraExtenso(resto);
             }
             return extenso;
         }
     }
 
+    public static void main(String[] args) {
+        System.out.println(paraExtenso(MAX_VALUE.negate().subtract(BigInteger.ONE)));
+    }
 }
